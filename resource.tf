5b54155deb2988d33a1a0fe15ad3cade8f8318bf
TEJPAL SINGH <18672@teamcomputers.com>
2024-11-08 10:02:35 +0530

    changes done in local laptop

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "rg-bulbul" {
-    name = "bulbul-rg"
-    location = "centralindia"
-  
+resource "azurerm_resource_group" "rg1" {
+  name = "rg1"
+  location = "eastus"
 }
+
