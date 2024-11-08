b0a44c5159ebcf0287b32c6bfe60b7810433c9c0
Tejpal88 <90080347+Tejpal88@users.noreply.github.com>
2024-11-08 10:16:11 +0530

    Update main.tf
    New rg and tag added in this

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg1" {
   name = "main-rg1"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name = "main-rg2"
+  location = "centralindia"
+  tag = "hello"
+}
+
